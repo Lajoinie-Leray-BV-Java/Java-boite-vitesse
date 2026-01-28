40899088bc298cb862401c32a7fa0eaea49eebe9
Elie Lajoinie <elielajoinie24@gmail.com>
2026-01-28 12:11:46 +0100

    feat: initialize project

---
+++ src/main/java/sdv/formation/gearbox/GearboxApplication.java
@@ -0,0 +1,13 @@
+package sdv.formation.gearbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sdv/formation/gearbox/GearboxApplicationTests.java
@@ -0,0 +1,13 @@
+package sdv.formation.gearbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GearboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

42a140a62519e18630732681254b3ac6717e05fa
Elie Lajoinie <elielajoinie24@gmail.com>
2026-01-28 16:58:52 +0100

    fix: commented files & fixed delete request

--- src/main/java/sdv/formation/gearbox/controllers/GearBoxController.java
+++ src/main/java/sdv/formation/gearbox/controllers/GearBoxController.java
@@ -12,29 +12,46 @@ import sdv.formation.gearbox.services.GearBoxService;
 import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("/gearBox")
 public class GearBoxController {
     private final GearBoxService _gearBoxService;
-    private final String _path = "/gearBox";
 
+    /**
+    * Initialise le controlleur avec le service de boîte de vitesses.
+    * @param gearBoxService Le service de boîte de vitesses à utiliser.
+    */
     public GearBoxController(GearBoxService gearBoxService) {
         _gearBoxService = gearBoxService;
     }
 
-    @GetMapping(_path)
+    /**
+    * Récupère toutes les boîtes de vitesses.
+    * @return Une réponse HTTP contenant la liste des boîtes de vitesses.
+    */
+    @GetMapping()
     public ResponseEntity<Iterable<GearBox>> getAllGearBoxes() {
         Iterable<GearBox> gearBoxes = _gearBoxService.getAll();
         return ResponseEntity.ok(gearBoxes);
     }
 
-    @GetMapping(_path + "/{id}")
+    /**
+    * Récupère une boîte de vitesses par son identifiant.
+    * @param id L'identifiant de la boîte de vitesses à récupérer.
+    * @return Une réponse HTTP contenant la boîte de vitesses si trouvée, sinon une réponse 404.
+    */
+    @GetMapping("/{id}")
     public ResponseEntity<GearBox> getGearBox(@PathVariable("id") Long id) {
         GearBox gearBox = _gearBoxService.getById(id).orElse(null);
         if (gearBox == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(gearBox);
     }
 
-    @PostMapping(_path)
+    /**
+    * Crée une nouvelle boîte de vitesses.
+    * @param createGearBoxRequest La requête de création de boîte de vitesses (optionnelle).
+    * @return Une réponse HTTP contenant la boîte de vitesses créée.
+    */
+    @PostMapping()
     public ResponseEntity<GearBox> createGearBox(@RequestBody Optional<CreateGearBoxRequest> createGearBoxRequest) {
         if (createGearBoxRequest.isPresent()) {
             try {
@@ -52,19 +69,35 @@ public class GearBoxController {
         return ResponseEntity.ok(createdGearBox);
     }
 
-    @PutMapping(_path + "/{id}/to/{targetGear}")
+    /**
+    * Met à jour la boîte de vitesses en changeant de vitesse.
+    * @param id L'identifiant de la boîte de vitesses à mettre à jour.
+    * @param targetGear La vitesse cible à laquelle passer.
+    * @return Une réponse HTTP contenant la boîte de vitesses mise à jour ou un message d'erreur.
+    */
+    @PutMapping("/{id}/to/{targetGear}")
     public ResponseEntity<?> swapGear(@PathVariable("id") Long id, @PathVariable("targetGear") GearsEnum targetGear) {
         try {
             GearBox gearBox = _gearBoxService.updateGear(id, targetGear);
+            if (gearBox == null) return ResponseEntity.notFound().build();
+
             return ResponseEntity.ok(gearBox);
         } catch (GearException gE) {
             return ResponseEntity.badRequest().body(gE.getMessage());
         }
     }
 
-    @DeleteMapping(_path + "/{id} ")
+    /**
+     * Supprime une boîte de vitesses par son identifiant.
+     * Nous n'avons pas utiliser @DeleteMapping car cela envoyait une erreur 405 Method Not Supported .
+     * @param id L'identifiant de la boîte de vitesses à supprimer.
+     * @return Une réponse HTTP indiquant le succès de l'opération.
+     * */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteGearBox(@PathVariable("id") Long id) {
-        _gearBoxService.deleteGearBox(id);
+        boolean isDeleted = _gearBoxService.deleteGearBox(id);
+        if (!isDeleted) return ResponseEntity.notFound().build();
+
         return ResponseEntity.noContent().build();
     }
 }

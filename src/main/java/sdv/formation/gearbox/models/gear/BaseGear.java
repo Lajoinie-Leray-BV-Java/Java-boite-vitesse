8370bf85acb9a4a072d0c9d83c04224f4d0e056a
Elie Lajoinie <elielajoinie24@gmail.com>
2026-01-28 17:03:24 +0100

    feat: comments to explain inheritance

--- src/main/java/sdv/formation/gearbox/models/gear/BaseGear.java
+++ src/main/java/sdv/formation/gearbox/models/gear/BaseGear.java
@@ -13,7 +13,10 @@ import sdv.formation.gearbox.enums.GearsEnum;
 @Entity
 @Data
 @NoArgsConstructor
+// Ajout d'une annotation d'héritage pour la stratégie pour que JPA puisse gérer les sous-classes et permettre de correctement changer les vitesses plus tard.
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+// Ajoute une colonne de "discrimination" pour identifier le type de chaque enregistrement dans la table.
+// Il était impossible de le mettre sur name dû à la contrainte d'unicité.
 @DiscriminatorColumn(name = "gear_type")
 public class BaseGear {
     @Id
